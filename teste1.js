6e469e1a11b97501e3202ed7d42bd73eef752d54
MesopotamiaAlpha <leao-gomes@hotmail.com>
2021-01-31 16:17:10 -0300

    Teste de import/Export

---
+++ teste1.js
@@ -0,0 +1,10 @@
+//Aqui estou iportando a variavel teste2 dentro do arquivo teste2.js
+import teste2  from "./teste2.js";
+
+//Para importar uma função preciso usar import {Nome_da_funcao} from "./teste2.js" e preciso usar obrigatoriamente o nome da mesma função
+
+var teste = "texto dentro do teste1"
+
+
+console.log(teste);
+console.log(teste2);    
---
+++ teste2.js
@@ -0,0 +1,10 @@
+var teste2 = "texto dentro do teste 2"
+
+
+//aqui estou exportando a variavel teste2,se eu declarar default quando for importar eu tenho que colocar o nome da variavel que vai receber no prox arquivo, caso eu usar somente o export eu preciso usar obrigatoriamente o mesmo nome de variavel
+export default teste2;
+
+//export teste2;
+
+
+//Caso eu quiser exportar uma função eu preciso colocar export function nomeFuncão();
